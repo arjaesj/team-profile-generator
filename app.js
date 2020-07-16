9bf6eab80a1849a8723d3294e23c3a401dc79638
arjae.sj <61751130+arjaesj@users.noreply.github.com>
2020-07-17 01:09:38 +0800

    coded app.js

--- app.js
+++ app.js
@@ -14,6 +14,260 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+
+const myTeamArray = []; //empty array for the user's team, members will be pushed when the user finished asnwering the inquirer prompts
+
+function myTeamPrompts() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Please type in your complete name.",
+          //validation function so the user will have to input a valid string for the name
+          validate: (response) => {
+            if (response !== "") {
+              return true;
+            } else {
+              return "Please type in a valid input characters.";
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "Please type in your employee ID.",
+          //validation function so the user will have to input a valid number for the ID 
+          validate: (response) => {
+            
+            // arjae: from stackoverflow
+            const responseValidNumber = response.match(/\d+g/);
+            
+            // const valid = response.match(/^[1-9]\d*$/); 
+            if (responseValidNumber) {
+              return true;
+            } else {
+              return "Please enter a valid number value from 0-9.";
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Please type in your email address.",
+          //validation function so the user will have to input a valid string for the email address
+          validate: (response) => {
+            if (response !== "") {
+              return true;
+            } else {
+              return "Please type in a valid input characters.";
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "Please type in your work phone number.",
+          //validation function so the user will have to input a valid number for the work phone number
+          validate: (response) => {
+
+            // arjae: from stackoverflow
+            const responseValidNumber = response.match(/\d+g/);
+
+            // const valid = response.match(/^[1-9]\d*$/);
+            if (valid) {
+              return true;
+            } else {
+              return "Please enter a valid number value from 0-9.";
+            }
+          },
+        },
+      ])
+      //promise handled/fulfilled with instantiating a new Employee called Manager with the inputs from the inquirer prompts above.
+      .then((answer) => {
+        const manager = new Manager(
+          answer.name,
+          answer.id,
+          answer.email,
+          answer.officeNumber
+        );
+        // data fromt the user input to be pushed to the myTeamArray array
+        teamArr.push(manager);
+        chooseTeam();
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+  // promp the manager to choose their team, picking from either engineer, or intern, also a choice to end adding employees.
+  function chooseTeam() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "userchoice",
+          message: "Which role would you like to add to your team?",
+          choices: [
+            "Engineer",
+            "Intern",
+            "I do not want to add anyone else to my team",
+          ],
+        },
+      ])
+      .then(({ userchoice }) => {
+        switch (userchoice) {
+          case "Engineer":
+            addEngineer();
+            break;
+          case "Intern":
+            addIntern();
+            break;
+          default:
+            createTeam();
+        }
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+  // funciton for adding the software engineers
+  function addEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Please provide your Engineer's name",
+          validate: (response) => {
+            if (response !== "") {
+              return true;
+            } else {
+              return "please enter a valid charater input";
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "Please provide your Engineer's employee ID",
+          validate: (response) => {
+            const valid = response.match(/^[1-9]\d*$/);
+            if (valid) {
+              return true;
+            } else {
+              return "You must endter a number between 1-10";
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Please provide your Engineer's email",
+          validate: (response) => {
+            if (response !== "") {
+              return true;
+            } else {
+              return "please enter a valid charater input";
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "github",
+          message: "Please Enter Your Engineer's GitHub Username:",
+          validate: (response) => {
+            if (response !== "") {
+              return true;
+            } else {
+              return "please enter a valid charater input";
+            }
+          },
+        },
+      ])
+      .then((answer) => {
+        const engineer = new Engineer(
+          answer.name,
+          answer.id,
+          answer.email,
+          answer.github
+        );
+        teamArr.push(engineer);
+        chooseTeam();
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+  // funciton for adding interns
+  function addIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Please provide Intern's your name",
+          validate: (response) => {
+            if (response !== "") {
+              return true;
+            } else {
+              return "please enter a valid charater input";
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "Please provide your Intern's employee ID",
+          validate: (response) => {
+            const valid = response.match(/^[1-9]\d*$/);
+            if (valid) {
+              return true;
+            } else {
+              return "You must endter a number between 1-10";
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Please provide Intern's your email",
+          validate: (response) => {
+            if (response !== "") {
+              return true;
+            } else {
+              return "please enter a valid charater input";
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "school",
+          message: "Please Enter the name of your Intern's School:",
+          validate: (response) => {
+            if (response !== "") {
+              return true;
+            } else {
+              return "please enter a valid charater input";
+            }
+          },
+        },
+      ])
+      .then((answer) => {
+        const intern = new Intern(
+          answer.name,
+          answer.id,
+          answer.email,
+          answer.school
+        );
+        teamArr.push(intern);
+        chooseTeam();
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
